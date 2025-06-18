5de920a3f260c37c5b98a679918b610ee0011eaa
msh-94 <alstjdgh2003@naver.com>
2025-06-18 17:09:32 +0900

    오후2교시

--- day16/실습11.js
+++ day16/실습11.js
@@ -17,12 +17,12 @@
 // let 합계 = 0;
 // function sumArray( num ){
 //     for( let i = 0; i <= num.length-1; i++){
-//         합계 += num[i]        
+//         합계 += num[i];        
 //     }
 //     return 합계; // 누적합계 반환 --> sumArray() 가 호출된곳으로 반환
 // }
 // sumArray(numbers);      // 함수 호출하고 반환값 변수에 저장 x
-// console.log(합계)
+// console.log(합계);
 
 // ---****
 // 실습 3: 가장 긴 단어 찾기
@@ -31,7 +31,7 @@
 // function findLongestWord( array ){
 //     let longWord = '';  // 가장 긴 단어를 저장하는 변수
 //     for( let i = 0; i <= array.length-1; i++){
-//         let word = array[i]
+//         let word = array[i];
 //         // 만약에 i번째 단어가 가장긴단어 보다 길면 가장긴단어 에 i번째 단어 대입한다.
 //         if( word.length > longWord.length ){ longWord = word; }
 //     } // for end
@@ -41,20 +41,20 @@
 // console.log( 결과 );
 
 
-// 실습 4: 게임 점수 관리하기
+// 실습 4: 게임 점수 관리하기   - 재귀함수 : 함수 안에서 현재 함수를 재호출
 // let userScore = 0; 전역 변수를 만드세요. gainScore() 함수는 userScore를 10 증가시키고, loseScore() 함수는 5 감소시킵니다.
 // gainScore()를 세 번, loseScore()를 한 번 호출한 뒤, 최종 점수를 콘솔에 출력하세요.
 // let userScore = 0;
 // function gainScore(){
-//     userScore += 10
+//     userScore += 10;
 // }
 // function loseScore(){
-//     userScore -= 5
+//     userScore -= 5;
 // }
 // gainScore();
 // gainScore();
 // gainScore();
-// loseScore()
+// loseScore();
 // console.log( userScore );
 
 //
@@ -68,7 +68,7 @@
 //         let 학생 = student[i];            
 //         if( max < 학생.score ){ 
 //             max = 학생.score;
-//             name = 학생.name                      
+//             name = 학생.name;                      
 //         }       
 //     }
 //     return name;
@@ -82,11 +82,11 @@
 // const products = [{ name: '노트북', price: 1200000, stock: 5 },{ name: '모니터', price: 350000, stock: 12 },{ name: '키보드', price: 80000, stock: 25 }];
 // function renderProducts(상품){
 //     for( let i = 0; i <= 상품.length-1; i++){
-//         let 재고 = 상품[i]
+//         let 재고 = 상품[i];
 //         document.write(`<div><h4>${재고.name}</h4> <p>${재고.price}</p> <p>${재고.stock}</p></div>`)
 //     }
 // }
-// renderProducts(products)
+// renderProducts(products);
 
 
 // 실습 7: 간단한 계산기
@@ -99,7 +99,7 @@
 // let num1 = Number(prompt('숫자1 : '));  
 // let num2 = Number(prompt('숫자2 : '));
 // let 기호 = prompt('+ 또는 - :');
-// calculator(num1 , num2 , 기호)
+// calculator(num1 , num2 , 기호);
 // let 결과 = calculator(num1 , num2 , 기호);
 // console.log(결과);
 
@@ -112,7 +112,7 @@
 // let todoList = ['장보기', '운동하기']
 // function rendList(목록){
 //     for( let i = 0; i <= 목록.length-1; i++){
-//         let 할일 = 목록[i]
+//         let 할일 = 목록[i];
 //         document.write(` <ul><li>${할일}</li></ul>`)
 //     }
 // }
@@ -127,10 +127,10 @@
 // 계산된 가격 문자열을 반환(return)하시오.
 // function getTicketPrice(age){
 //     if( age >= 20 ){
-//         return '10,000원'
+//         return '10,000원';
 //     }else if( age >= 8 ){
-//         return '5,000원'
-//     }else{ return '무료'}
+//         return '5,000원';
+//     }else{ return '무료';}
 // }
 // let 가격 = getTicketPrice(22);
-// console.log(가격)
+// console.log(가격);
