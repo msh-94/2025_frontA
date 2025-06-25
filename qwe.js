e47329382bdf405286cce8859abbe208dffe629f
msh-94 <alstjdgh2003@naver.com>
2025-06-25 17:36:47 +0900

    오후3교시

--- qwe.js
+++ qwe.js
@@ -11,23 +11,24 @@ function 저장하기(products){
 
 function 등록함수(){
     const nameInput = document.querySelector('#nameInput');
-    const priceInput = document.querySelector('#priceInput');
+    const ppriceInput = document.querySelector('#ppriceInput');
     const imgInput = document.querySelector('#imgInput');
     const areaInput = document.querySelector('#areaInput');
     //
     let name = nameInput.value;
-    let price = priceInput.value;
+    let price = ppriceInput.value;
     let img = imgInput.files[0];
     let area = areaInput.value;
     //
     let products = 가져오기();
     let pcode = products.length == 0 ? 1 : products[products.length-1].pcode + 1;
-    // 
-    let obj = { pcode : pcode , 이름 : name , 가격 : price , 이미지 : img ? URL.createObjectURL(img) : '' , 설명 : area}
+    // e
+    let obj = { pcode : pcode , 이름 : name , 가격 : Number(price) ,
+        이미지 : img ? URL.createObjectURL(img) : 'http://placehold.co/100x100' , 설명 : area}
     //
     products.push(obj);
-    출력함수()
     저장하기(products)
+    출력함수()    
 }
 출력함수()
 function 출력함수(){
@@ -36,12 +37,12 @@ function 출력함수(){
     let html = '';
     for(let i = 0; i <= products.length-1; i++){
         제품 = products[i]
-        html = `<tr>
+        html += `<tr>
                         <td>${제품.이름}</td>
                         <td>${제품.가격}</td>
                         <td>${제품.설명}</td>
                         <td><img src="${제품.이미지}"/></td>
-                        <td><button onclick="삭제함수(${제품.pcode})"></button></td>
+                        <td><button onclick="삭제함수(${제품.pcode})">삭제</button></td>
                     </tr>`
     }
     bodyIn.innerHTML = html;
